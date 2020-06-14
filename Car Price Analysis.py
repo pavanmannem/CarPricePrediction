9f7be6de5aac35ba6edc34f2d8a5efd9b8f8020e
Pavan Mannem <66274083+pavanmannem@users.noreply.github.com>
2020-06-14 13:06:46 -0700

    Update Car Price Analysis.py

--- Car Price Analysis.py
+++ Car Price Analysis.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,33 +7,18 @@ pd.set_option('display.max_columns', None)
 
 # # Data Collection
 
-# In[2]:
-
-
 data = pd.read_csv('car_price.csv')
 data.head()
-
-
-# In[3]:
-
 
 company = data['CarName'].apply(lambda x : x.split(' ')[0])
 data.insert(2,"CompanyName",company)
 data.drop(['CarName'],axis = 1, inplace=True)
 data.drop(['car_ID'],axis=1,inplace=True)
 data.head()
-
-
-# In[4]:
-
-
 data.price.describe()
 
 
 # # Data Cleaning
-
-# In[5]:
-
 
 #There are mispellings in the data, need to correct them
 data.CompanyName.unique()
@@ -53,27 +32,12 @@ rename('toyouta','toyota')
 rename('vokswagen','volkswagen')
 
 data.CompanyName.unique()
-
-
-# # Exploratory Data Analysis
-
-# In[6]:
-
-
 plt.title('Price Distribution')
 sns.distplot(data.price)
-
-
-# In[7]:
-
 
 df = pd.DataFrame(data.groupby(['CompanyName'])['price'].mean().sort_values(ascending = False))
 df.plot.bar()
 plt.title('Avg Price by Company')
-
-
-# In[8]:
-
 
 def plot_price(attr1,attr2,attr3):
     sns.pairplot(data, x_vars=[attr1,attr2,attr3], y_vars='price',height=4, aspect=1, kind='scatter')
@@ -82,9 +46,9 @@ def plot_price(attr1,attr2,attr3):
 plot_price('enginesize', 'horsepower', 'highwaympg')
 
 
-# ## We can identify which attributes of the vehicle affect car price. Doing this for all attributes, we make a new dataframe using only highly correlated variables
+## We can identify which attributes of the vehicle affect car price. 
+## Doing this for all attributes, we make a new dataframe using only highly correlated variables
 
-# In[9]:
 
 
 # Derived property using a common formula for fuel economy
@@ -94,9 +58,6 @@ car_final = data[['price', 'fueltype', 'aspiration','carbody', 'drivewheel','whe
                   'curbweight', 'enginetype', 'cylindernumber', 'enginesize', 'boreratio','horsepower', 
                     'fueleconomy', 'carlength','carwidth']]
 car_final.head()
-
-
-# In[10]:
 
 
 # In order to input categorical data into the model we need to convert the colums to binary
@@ -115,17 +76,10 @@ car_final = create_dummies('drivewheel',car_final)
 car_final = create_dummies('enginetype',car_final)
 car_final = create_dummies('cylindernumber',car_final)
 
-
-# In[23]:
-
-
 car_final.head()
 
 
 # # Modeling
-
-# In[41]:
-
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -140,10 +94,6 @@ x_train = data_train
 
 y_test = data_test.pop('price')
 x_test = data_test
-
-
-# In[51]:
-
 
 #Applying a multiple linear regression model 
 linReg = LinearRegression()
